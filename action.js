6524585b46b227777b29070fa32ab887e5d544d5
@loki157 <lokesh2001sankar@gmail.com>
2022-11-10 23:46:45 +0530

    second commit

--- action.js
+++ action.js
@@ -1,3 +1,4 @@
+var keyWords;
 let respondApi;
 getApi();
 async function getApi() {
@@ -11,5 +12,29 @@ function tableData(data) {
   data.forEach((element) => {
     const row = document.createElement("tr");
     const userId = document.createElement("td");
+    const it = document.createElement("td");
+    const title = document.createElement("td");
+    const body = document.createElement("td");
+
+    userId.textContent = element.userId;
+    it.textContent = element.id;
+    title.textContent = element.title;
+    body.textContent = element.body;
+
+    row.appendChild(userId);
+    row.appendChild(it);
+    row.appendChild(title);
+    row.appendChild(body);
+    getTable.append(row);
   });
+}
+//searchFilter(respondApi);
+function filterData(data, keyWords) {
+  keyWords = document.querySelector(".inp");
+  return data.filter((dataStat) => dataStat.includes(keyWords));
+  //return keywordFil;
+}
+
+function searchFilter() {
+  console.log(filterData(respondApi, keyWords));
 }
